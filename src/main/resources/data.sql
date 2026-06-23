f93992cdb7a1133845fd8bc8345efc552ff77084
JLopezore <j.lopezlopez1004@gmail.com>
2026-06-23 12:08:30 -0600

    add: datos en bd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-TRUNCATE TABLE videojuegos RESTART IDENTITY CASCADE;
-
 -- RPGs y Acción/Aventura
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('The Legend of Zelda: Breath of the Wild', 'Nintendo Switch', 2017, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('The Legend of Zelda: Tears of the Kingdom', 'Nintendo Switch', 2023, false);
@@ -10,6 +8,9 @@ INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUE
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Red Dead Redemption 2', 'PS4', 2018, false);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Cyberpunk 2077', 'PC', 2020, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Persona 5 Royal', 'PS4', 2019, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Mass Effect Legendary Edition', 'PC', 2021, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Horizon Zero Dawn', 'PS4', 2017, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Final Fantasy VII Remake', 'PS4', 2020, false);
 
 -- Plataformas y Metroidvanias
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Hollow Knight', 'PC', 2017, true);
@@ -17,18 +18,48 @@ INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUE
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Super Mario Bros. Wonder', 'Nintendo Switch', 2023, false);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Metroid Dread', 'Nintendo Switch', 2021, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Celeste', 'PC', 2018, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Ori and the Will of the Wisps', 'Xbox One', 2020, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Dead Cells', 'PC', 2018, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Cuphead', 'PC', 2017, true);
 
 -- Shooters y Acción Rápida
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('DOOM Eternal', 'PC', 2020, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Halo Infinite', 'Xbox Series X', 2021, false);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Returnal', 'PS5', 2021, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Overwatch 2', 'PC', 2022, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Call of Duty: Modern Warfare II', 'PS5', 2022, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Destiny 2', 'PC', 2017, true);
 
 -- Indies Populares
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Hades', 'Nintendo Switch', 2020, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Stardew Valley', 'PC', 2016, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Vampire Survivors', 'PC', 2022, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Undertale', 'PC', 2015, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Disco Elysium', 'PC', 2019, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Slay the Spire', 'PC', 2019, true);
 
 -- Clásicos Modernos
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Bloodborne', 'PS4', 2015, true);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Sekiro: Shadows Die Twice', 'PC', 2019, false);
 INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('NieR: Automata', 'PS4', 2017, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Dark Souls III', 'PC', 2016, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('BioShock Infinite', 'PC', 2013, true);
+
+-- Deportes y Carreras
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Forza Horizon 5', 'Xbox Series X', 2021, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Mario Kart 8 Deluxe', 'Nintendo Switch', 2017, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Gran Turismo 7', 'PS5', 2022, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Rocket League', 'PC', 2015, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('FIFA 23', 'PS5', 2022, false);
+
+-- Estrategia y Simulación
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Civilization VI', 'PC', 2016, false);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Age of Empires II: Definitive Edition', 'PC', 2019, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('The Sims 4', 'PC', 2014, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Cities: Skylines', 'PC', 2015, false);
+
+-- Retro Classics
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Super Mario 64', 'Nintendo 64', 1996, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('The Legend of Zelda: Ocarina of Time', 'Nintendo 64', 1998, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Castlevania: Symphony of the Night', 'PlayStation', 1997, true);
+INSERT INTO videojuegos (titulo, plataforma, anio_lanzamiento, completado) VALUES ('Chrono Trigger', 'SNES', 1995, true);
